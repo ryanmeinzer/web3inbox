a2b361bc797d9d7abbcaf2f814ff75837110e264
Ryan Meinzer <ryanmeinzer@Ryans-MacBook-Pro.local>
2022-09-29 21:51:26 -0700

    adding basic tests

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -3,3 +3,40 @@ const ganache = require('ganache-cli')
 const Web3 = require('web3')
 const web3 = new Web3(ganache.provider())
 
+
+
+
+
+
+
+
+
+
+
+
+
+// Note - holding below for safekeeping
+// class Car {
+//     park() {
+//         return 'stopped'
+//     }
+
+//     drive() {
+//         return 'vroom'
+//     }
+// }
+
+// let car
+
+// beforeEach(() => {
+//     car = new Car()
+// })
+
+// describe('Car Class Test', () => {
+//     it('can park', () => {
+//         assert.equal(car.park(), 'stopped')
+//     })
+//     it('can drive', () => {
+//         assert.equal(car.drive(), 'vroom')
+//     })
+// })
