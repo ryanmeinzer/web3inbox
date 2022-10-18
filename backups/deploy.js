2ce199c13fa4ceee8b26d58f37eefbc63f098b6f
Ryan Meinzer <ryanmeinzer@Ryans-MacBook-Pro.local>
2022-10-17 22:22:14 -0700

    adding Lottery contract

---
+++ backups/deploy.js
@@ -0,0 +1,27 @@
+require('dotenv').config()
+const myAccountMnemonic = process.env.MY_ACCOUNT_MNEMONIC
+const HDWalletProvider = require('@truffle/hdwallet-provider')
+const Web3 = require('web3')
+const {abi, evm} = require('./compile')
+
+const provider = new HDWalletProvider(
+    `${myAccountMnemonic}`,
+    'https://goerli.infura.io/v3/78389dcce0d54faaa7e109f5111845f0'
+)
+const web3 = new Web3(provider)
+
+const deploy = async () => {
+    const accounts = await web3.eth.getAccounts()
+
+    console.log('Attempting to deploy from account', accounts[0])
+
+    const result = await new web3.eth.Contract(abi)
+        .deploy({data: evm.bytecode.object, arguments: ['Hi there!']})
+        .send({gas: '1000000', from: accounts[0]});
+
+    console.log('Contract deployed to', result.options.address)
+    // prevent a hanging deployment
+    provider.engine.stop()
+}
+
+deploy()
