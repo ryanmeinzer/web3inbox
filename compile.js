daeb628c266f287b92f1e4c6ea199f7fb259ccf7
Ryan Meinzer <ryanmeinzer@Ryans-MacBook-Pro.local>
2022-10-17 22:57:53 -0700

    reverting to only inbox contract, without lottery

--- compile.js
+++ compile.js
@@ -1,8 +1,26 @@
-const path = require("path");
-const fs = require("fs");
-const solc = require("solc");
+const path = require('path')
+const fs = require('fs')
+const solc = require('solc')
 
-const lotteryPath = path.resolve(__dirname, "contracts", "Lottery.sol");
-const source = fs.readFileSync(lotteryPath, "utf8");
+const inboxPath = path.resolve(__dirname, 'contracts', 'Inbox.sol')
+const source = fs.readFileSync(inboxPath, 'utf8')
 
-module.exports = solc.compile(source, 1).contracts[":Lottery"];
+const input = {
+    language: 'Solidity',
+    sources: {
+        'Inbox.sol': {
+            content: source,
+        },
+    },
+    settings: {
+        outputSelection: {
+            '*': {
+                '*': ['*'],
+            },
+        },
+    },
+}
+
+module.exports = JSON.parse(solc.compile(JSON.stringify(input))).contracts[
+    'Inbox.sol'
+].Inbox
